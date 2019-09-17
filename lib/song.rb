6c188443d0939e3438fcc9dedf20cf067ac962b8
Sarah H <sarahrose.henderson@gmail.com>
2019-09-17 21:48:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,30 +1,52 @@
 class Song
-  @@count = 0
-  @@artists = []
-  @@genres = []
-
   attr_accessor :name, :artist, :genre
 
-def initialize(song_name, name, artist)
-  @@count += 1
-  @name = song_name
-  @artist = artist
-  @genre = genre
-  @@genres << genre
-  @@artists << artist
-end
+  @@count = 0
+  @@genres = []
+  @@artists = []
 
-def self.artists
-  @@artist.uniq!
-end
-def self.count
-  @@count
-end
+  def initialize(song_name, artist, genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
 
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq!
+  end
+
+  def self.artists
+    @@artists.uniq!
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre] #if the genre is already in the hash
+        genre_count[genre] += 1
+      else #if it's a new genre
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist] #if the artist is already in the hash
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
 end
-# new_song = Song.new
-# new_song.name = "99 Problems"
-# new_song.artist = "Jay Z"
-# new_song.genre = "HipHop"
-#
-# new_song.artist
