84429f2fa233f8749a87e3d56f49a960ea9c7040
Sarah H <sarahrose.henderson@gmail.com>
2019-09-17 21:40:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
 
   attr_accessor :name, :artist, :genre
 
-def initialize(song, name, artist)
+def initialize(song_name, name, artist)
   @@count += 1
   @name = song_name
   @artist = artist
